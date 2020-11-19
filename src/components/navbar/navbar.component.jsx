828b30f5bb905cf78cfa914b8f834887dd4d1915
Moses Kamara <kamara.moses@yahoo.com>
2020-11-18 21:08:08 -0800

    navbar component to the components folder

---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import "./navbar.css";
+
+const MyNavbar = () => {
+  return (
+    <div>
+      <Navbar
+        fixed="top"
+        variant="light"
+        expand="md"
+        className="animate-navbar nav-theme justify-content-between"
+      >
+        <div>
+          <Navbar.Brand href="#home">
+            <h1>Moses Kamara</h1>
+          </Navbar.Brand>
+        </div>
+        <div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto ">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#projects">Portfolio</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
+    </div>
+  );
+};
+
+export default MyNavbar;
